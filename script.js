28b4f5647cb24c6d16c1c6e1091be39ca021ba1f
Rishabh Ghanty <99227180+rghanty@users.noreply.github.com>
2024-06-13 11:54:23 +0530

    Added webpage scripts and images file

---
+++ script.js
@@ -0,0 +1,19 @@
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky',window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
